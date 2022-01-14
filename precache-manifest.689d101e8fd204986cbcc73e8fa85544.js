9d383cf1c512b717260c5e2f2818d866daa15c61
PieroVicenti <p.vicenti@outlook.com>
2022-01-14 16:04:16 +0000

    Updates

---
+++ precache-manifest.689d101e8fd204986cbcc73e8fa85544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da00ff05bae1402bd60426522bef3284",
+    "url": "/AdvancedToDoList/index.html"
+  },
+  {
+    "revision": "53160a1ebcaffbb8de6c",
+    "url": "/AdvancedToDoList/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "a8d6aee522f1ba78c0ff",
+    "url": "/AdvancedToDoList/static/js/2.565d5f76.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/AdvancedToDoList/static/js/2.565d5f76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53160a1ebcaffbb8de6c",
+    "url": "/AdvancedToDoList/static/js/main.eb0aab9f.chunk.js"
+  },
+  {
+    "revision": "948a4e12b87c2e4feb2b",
+    "url": "/AdvancedToDoList/static/js/runtime-main.2744b55e.js"
+  }
+]);
